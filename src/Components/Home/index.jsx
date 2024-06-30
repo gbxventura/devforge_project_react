5691913e37c9b63b25264ea9224fdaae33f15652
Gabriel Ventura <89049503+gbxventura@users.noreply.github.com>
2024-06-30 19:55:30 -0300

    Update index.jsx

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import styles from "./Home.module.css"
-import img_home from "../../assets/imgs/home-img.png";
+import React, { useEffect } from 'react';
+import ScrollReveal from 'scrollreveal';
+import styles from './Home.module.css';
+import img_home from '../../assets/imgs/home-img.png';
 
 const Home = () => {
+    useEffect(() => {
+        const sr = ScrollReveal({
+            origin: 'bottom',
+            distance: '20px',
+            duration: 1000,
+            reset: true
+        });
+
+        sr.reveal(`.${styles.home_content} h4`, { delay: 200 });
+        sr.reveal(`.${styles.home_content} h1`, { delay: 400 });
+        sr.reveal(`.${styles.home_content} p`, { delay: 600 });
+        sr.reveal(`.${styles.home_content} .${styles.btn_orcamento}`, { delay: 800 });
+        sr.reveal(`.${styles.home} .img`, { delay: 1000 });
+    }, []);
+
     return (
-        <>
-            <div className={styles.home}>
-                <div className={styles.home_content}>
-                    <h4>Criação de Sites Profissionais</h4>
-                    <h1>Transformando Ideias em Projetos Profissionais</h1>
-                    <p>Somos uma agência especializada na criação de sites e plataformas
-                        digitais para empresas, focada em projetar, desenvolver e gerenciar
-                        soluções profissionais para a web.</p>
-                    <a href="#orcamento" className={styles.btn_orcamento}>Quero um orçamento</a>
-                </div>
-                <div class="img">
-                    <img src={img_home} alt="imagem-principal" />
-                </div>
+        <div className={styles.home}>
+            <div className={styles.home_content}>
+                <h4>Criação de Sites Profissionais</h4>
+                <h1>Transformando Ideias em Projetos Profissionais</h1>
+                <p>
+                    Somos uma agência especializada na criação de sites e plataformas digitais para empresas,
+                    focada em projetar, desenvolver e gerenciar soluções profissionais para a web.
+                </p>
+                <a href="#orcamento" className={styles.btn_orcamento}>Quero um orçamento</a>
             </div>
-        </>
+            <div className="img">
+                <img src={img_home} alt="imagem-principal" />
+            </div>
+        </div>
     );
 }
 
