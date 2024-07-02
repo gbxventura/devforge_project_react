672e68409bf5dfeab82c8ff51da4d65e7039d759
Gabriel Ventura <89049503+gbxventura@users.noreply.github.com>
2024-07-02 19:43:28 -0300

    a

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import HomePg from './HomePg';
-
-
-
 const Login = () => {
-    return (
-        <HomePg />
-);
-}
-
-
+  return (
+    <>
+      <h1>Página de login</h1>
+      <label htmlFor=''>Login</label>
+      <input type='text' />
+      <label htmlFor=''>Senha</label>
+      <input type='text' />
+    </>
+  );
+};
 
 export default Login;
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,18 +1,16 @@
-import React from "react";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import HomePg from "./Pages/HomePg";
-import Login from "./Pages/Login";
-import Pg404 from "./Pages/Pg404";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePg from './Pages/HomePg';
+import Login from './Pages/Login';
+import Pg404 from './Pages/Pg404';
 
 export default function AppRoutes() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<HomePg />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/*" element={<Pg404 />} />
-			</Routes>
-		</BrowserRouter>
-	)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePg />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/*' element={<Pg404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
