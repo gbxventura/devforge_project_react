03f87be82657c01d7247b0c610eb737f4dd08d7e
Gabriel Ventura <162187292+gbxventuraedu@users.noreply.github.com>
2024-07-10 13:51:33 -0300

    a

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -6,38 +6,6 @@ const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000'; // Fallb
 const Admin = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchAdminData = async () => {
-      const token = localStorage.getItem('token');
-      if (!token) {
-        navigate('/login');
-        return;
-      }
-
-      try {
-        const response = await fetch(`${apiUrl}/api/admin`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (response.status !== 200) {
-          navigate('/login');
-        } else {
-          const data = await response.json();
-          console.log(data);
-        }
-      } catch (error) {
-        console.error('Erro ao buscar dados de admin:', error);
-        navigate('/login');
-      }
-    };
-
-    fetchAdminData();
-  }, [navigate]);
-
   return (
     <div>
       <h2 className={styles.h2admin}>Página de Admin</h2>
