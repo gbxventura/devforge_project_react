a903a437be091fe09d9c6f8ad45fa102240f2914
Gabriel Ventura <89049503+gbxventura@users.noreply.github.com>
2024-07-10 09:42:06 -0300

    Update Admin.jsx

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styles from './Admin.module.css';
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000'; // Fallback para desenvolvimento local
 
 const Admin = () => {
   const navigate = useNavigate();
@@ -13,8 +15,6 @@ const Admin = () => {
       }
 
       try {
-        const apiUrl =
-          import.meta.env.VITE_API_URL || 'https://fallback-api-url.com';
         const response = await fetch(`${apiUrl}/api/admin`, {
           method: 'GET',
           headers: {
@@ -40,7 +40,7 @@ const Admin = () => {
 
   return (
     <div>
-      <h2>Página de Admin</h2>
+      <h2 className={styles.h2admin}>Página de Admin</h2>
     </div>
   );
 };
