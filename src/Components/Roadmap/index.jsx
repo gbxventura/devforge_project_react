129f76f7a1c6a66270eb48a61e8e353b62a0905e
Gabriel Ventura <89049503+gbxventura@users.noreply.github.com>
2024-07-11 18:29:21 -0300

    resolução da responsividade

--- src/Components/Roadmap/index.jsx
+++ src/Components/Roadmap/index.jsx
@@ -1,8 +1,6 @@
-import styles from './Roadmap.module.css';
-import card1 from '../../assets/imgs/card1.png';
-import card2 from '../../assets/imgs/card2.png';
-import card3 from '../../assets/imgs/card3.png';
+import React from 'react';
 import CenterMode from './CenterMode';
+
 const Roadmap = () => {
   return <CenterMode />;
 };
