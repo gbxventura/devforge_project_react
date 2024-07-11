ff93d0ca0ba5c3f2b71ede9f1efbe8f4af44e263
Gabriel Ventura <89049503+gbxventura@users.noreply.github.com>
2024-07-11 18:41:52 -0300

    roadmap config

--- src/Components/Portifolio/index.jsx
+++ src/Components/Portifolio/index.jsx
@@ -8,35 +8,63 @@ import portfolio5 from '../../assets/imgs/protifolio10.jpg';
 import portfolio6 from '../../assets/imgs/protifolio11.jpg';
 
 const Portfolio = () => {
-    const portfolioItems = [
-        { img: portfolio6, title: 'Projeto Web', description: 'Lorem, ipsum dolor sit amet consectetur' },
-        { img: portfolio5, title: 'Projeto Web', description: 'Lorem, ipsum dolor sit amet consectetur' },
-        { img: portfolio1, title: 'Projeto Web', description: 'Lorem, ipsum dolor sit amet consectetur' },
-        { img: portfolio2, title: 'Projeto Web', description: 'Lorem, ipsum dolor sit amet consectetur' },
-        { img: portfolio3, title: 'Projeto Web', description: 'Lorem, ipsum dolor sit amet consectetur' },
-        { img: portfolio4, title: 'Projeto Web', description: 'Lorem, ipsum dolor sit amet consectetur' }
-    ];
+  const portfolioItems = [
+    {
+      img: portfolio6,
+      title: 'Projeto Web',
+      description: 'Lorem, ipsum dolor sit amet consectetur',
+    },
+    {
+      img: portfolio5,
+      title: 'Projeto Web',
+      description: 'Lorem, ipsum dolor sit amet consectetur',
+    },
+    {
+      img: portfolio1,
+      title: 'Projeto Web',
+      description: 'Lorem, ipsum dolor sit amet consectetur',
+    },
+    {
+      img: portfolio2,
+      title: 'Projeto Web',
+      description: 'Lorem, ipsum dolor sit amet consectetur',
+    },
+    {
+      img: portfolio3,
+      title: 'Projeto Web',
+      description: 'Lorem, ipsum dolor sit amet consectetur',
+    },
+    {
+      img: portfolio4,
+      title: 'Projeto Web',
+      description: 'Lorem, ipsum dolor sit amet consectetur',
+    },
+  ];
 
-    return (
-        <section className={styles.portfolio} id="portfolio">
-            <div className={styles.mainText}>
-                <p>Portifólio</p>
-                <h2>Ultimos <span>Projetos</span></h2>
+  return (
+    <section className={styles.portfolio} id='portfolio'>
+      <div className={styles.mainText}>
+        <p>Portifólio</p>
+        <h2>
+          Ultimos <span>Projetos</span>
+        </h2>
+      </div>
+      <div className={styles.portfolioContent}>
+        {portfolioItems.map((item, index) => (
+          <div className={styles.row} key={index}>
+            <img src={item.img} alt={`portfolio-${index + 1}`} />
+            <div className={styles.layer}>
+              <h5>{item.title}</h5>
+              <p>{item.description}</p>
+              <a href='#contact'>
+                <i className='bx bx-link-external'></i>
+              </a>
             </div>
-            <div className={styles.portfolioContent}>
-                {portfolioItems.map((item, index) => (
-                    <div className={styles.row} key={index}>
-                        <img src={item.img} alt={`portfolio-${index + 1}`} />
-                        <div className={styles.layer}>
-                            <h5>{item.title}</h5>
-                            <p>{item.description}</p>
-                            <a href="#contact"><i className="bx bx-link-external"></i></a>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </section>
-    );
-}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default Portfolio;
--- src/Components/Roadmap/CenterMode.jsx
+++ src/Components/Roadmap/CenterMode.jsx
@@ -1,7 +1,8 @@
 import Slider from 'react-slick';
-import card1 from '../../assets/imgs/portifolio8.png';
-import card2 from '../../assets/imgs/protifolio11.jpg';
-import card3 from '../../assets/imgs/protifolio9.jpg.jpg';
+import card1 from '../../assets/imgs/roadmapImgs/1.png';
+import card2 from '../../assets/imgs/roadmapImgs/2.png';
+import card3 from '../../assets/imgs/roadmapImgs/3.png';
+import card4 from '../../assets/imgs/roadmapImgs/4.png';
 import styles from './Roadmap.module.css';
 
 function CenterMode() {
@@ -51,15 +52,7 @@ function CenterMode() {
         </div>
         <div>
           <h3>Título</h3>
-          <img src={card1} alt='Card 1' />
-        </div>
-        <div>
-          <h3>Título</h3>
-          <img src={card2} alt='Card 2' />
-        </div>
-        <div>
-          <h3>Título</h3>
-          <img src={card3} alt='Card 3' />
+          <img src={card4} alt='Card 1' />
         </div>
       </Slider>
     </div>
