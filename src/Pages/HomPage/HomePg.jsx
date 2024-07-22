95ddd0edabe92a333707460912c5b14cd653f0ca
eduardodasilva7 <124392594+eduardodasilva7@users.noreply.github.com>
2024-07-22 14:29:25 -0300

    a

--- src/Pages/HomPage/HomePg.jsx
+++ src/Pages/HomPage/HomePg.jsx
@@ -18,7 +18,7 @@ const HomePg = () => {
       <Portifolio />
       <Orcamento />
       <Footer />
-      <a href="https://wa.me/5511980211030?text=Ol%C3%A1!%20Gostaria%20de%20saber%20mais%20sobre%20os%20servi%C3%A7os%20de%20cria%C3%A7%C3%A3o%20de%20web%20projetos%20do%20DevForge.%20Como%20podemos%20come%C3%A7ar%3F%F0%9F%98%8A" className="whats-link">
+      <a href="https://wa.me/5511980211030?text=Ol%C3%A1!%20Gostaria%20de%20saber%20mais%20sobre%20os%20servi%C3%A7os%20de%20cria%C3%A7%C3%A3o%20de%20web%20projetos%20do%20DevF" className="whats-link">
       <i className="bx bxl-whatsapp" id="icon"></i>
     </a>
 
